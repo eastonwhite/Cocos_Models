a51b62229b365ecb49a1e8fcd0312e580fb3676f
erwhite1 <eastonrwhite@gmail.com>
2014-04-10 14:53:49 -0700

    Added comments

--- Cocos_MuM.R
+++ Cocos_MuM.R
@@ -3,12 +3,12 @@
 
 #this script contains codes for basic analyses using ADMB in R for Cocos data set
 
+#require pacakges
 require(glmmADMB)
 require(ggplot2)
 require(MuMIn)
 
-#call in data 
-
+#call in data and run a number of filters
 main=read.csv('FINAL_COMPLETE_main_species_data_EW.csv',header=T,sep=',')
 cocos=subset(main,main$IslandCode==1)
 cocos=cocos[,1:38]
@@ -43,10 +43,11 @@ cocos$StudyMonth<-as.numeric(cocos$StudyMonth)
 
 cocos=na.omit(cocos[,c(3,7,8,17,19,20,23,12,22,25:32)])
 
+#define seasonality terms
 cocos$SIN_TIME=sin(2*pi*cocos$StudyJulianDate/365.25)
 cocos$COS_TIME=cos(2*pi*cocos$StudyJulianDate/365.25)
 
-#####################################################
+#########################################################################################################
 #run basic NB GLM with MuMIn
 require(MASS)
 EagleRays_NB_GLM_theta=glm.nb(formula=EagleRays~StudyYear+CurrentCode+SeaTempF+VisibilityFt+ElNinoIndex+SIN_TIME+COS_TIME+DiverCode+SiteCode,data=na.omit(cocos))
@@ -61,7 +62,7 @@ EagleRays_NB_GLM_bestmodel <- glm(formula=EagleRays~StudyYear+SeaTempF+Visibilit
 
 save(EagleRays_NB_GLM_mumin,file='EagleRays_NB_GLM_mumin.Rdata')
 
-##############
+#########################################################################################################
 #run GLMM negative binomial
 EagleRays_NB_GLMER_theta=glmer.nb(formula=EagleRays~StudyYear+CurrentCode+SeaTempF+VisibilityFt+ElNinoIndex+SIN_TIME+COS_TIME+(1|DiverCode)+(1|SiteCode),data=na.omit(cocos))
 
@@ -75,9 +76,9 @@ EagleRays_NB_GLMER_mumin$dev_exp=100*(EagleRays_NB_GLMER_mumin$deviance[nrow(Eag
 EagleRays_NB_GLMER_bestmodel <- glmer(formula=EagleRays~StudyYear+SeaTempF+VisibilityFt+ElNinoIndex+(1|DiverCode)+(1|SiteCode),family=negative.binomial(0.3995),data=na.omit(cocos))
 
 save(EagleRays_NB_GLMER_mumin,file='EagleRays_NB_GLMER_mumin.Rdata')
-##########################################
 
-#GLMM with nbinom
+#########################################################################################################
+#GLMM with nbinom in glmmadmb
 
 EagleRays_ZINB_GLMM_full_model <- glmmadmb(formula=EagleRays~StudyYear+CurrentCode+SeaTempF+VisibilityFt+ElNinoIndex+SIN_TIME+COS_TIME+(1|DiverCode)+(1|SiteCode),data=na.omit(cocos),family="nbinom",zeroInflation=TRUE)
 EagleRays_ZINB_GLMM_mumin <- dredge(ZINB,rank="AIC",subset=(COS_TIME | !SIN_TIME) && (!COS_TIME | SIN_TIME))
